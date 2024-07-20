f947de686c8ff5910fc485f8ec75059ccd1d1514
Sergio Bazan <drakeb2008@hotmail.com>
2024-07-20 13:50:24 -0500

    add booking item configuration

--- ParkingALot.Infrastructure/Configurations/BookingConfiguration.cs
+++ ParkingALot.Infrastructure/Configurations/BookingConfiguration.cs
@@ -1,5 +1,54 @@
-﻿namespace ParkingALot.Infrastructure.Configurations;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ParkingALot.Domain.Bookings;
+using ParkingALot.Domain.Drivers;
+using ParkingALot.Domain.ParkingLotOwners;
+using ParkingALot.Domain.Shared;
 
-internal class BookingConfiguration
+namespace ParkingALot.Infrastructure.Configurations;
+
+internal sealed class BookingConfiguration : IEntityTypeConfiguration<Booking>
 {
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.ToTable("bookings");
+
+        builder.HasKey(booking => booking.Id);
+
+        builder.OwnsOne(booking => booking.PriceForPeriod, priceBuilder =>
+        {
+            priceBuilder.Property(price => price.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.ServicesPrice, priceBuilder =>
+        {
+            priceBuilder.Property(price => price.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.PointsDiscount, priceBuilder =>
+        {
+            priceBuilder.Property(price => price.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.TotalPrice, priceBuilder =>
+        {
+            priceBuilder.Property(price => price.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.Range);
+
+        builder
+            .HasOne<Driver>()
+            .WithMany()
+            .HasForeignKey(booking => booking.DriverId);
+
+        builder
+            .HasOne<ParkingLot>()
+            .WithMany()
+            .HasForeignKey(booking => booking.ParkingLotId);
+    }
 }
---
+++ ParkingALot.Infrastructure/Configurations/BookingItemConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ParkingALot.Domain.Bookings;
+using ParkingALot.Domain.ParkingLotOwners;
+using ParkingALot.Domain.Shared;
+
+namespace ParkingALot.Infrastructure.Configurations;
+
+internal sealed class BookingItemConfiguration : IEntityTypeConfiguration<BookingItem>
+{
+    public void Configure(EntityTypeBuilder<BookingItem> builder)
+    {
+        builder.ToTable("booking_items");
+
+        builder.HasKey(b => b.Id);
+
+        builder.OwnsOne(b => b.Price, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder
+            .HasOne<Booking>()
+            .WithMany()
+            .HasForeignKey(b => b.BookingId);
+
+        builder
+            .HasOne<Service>()
+            .WithOne()
+            .HasForeignKey<BookingItem>(b => b.ServiceId);
+    }
+}
